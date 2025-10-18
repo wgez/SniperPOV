f6153620a4403f0e93292d1dcb643b00b792e17d
wgez <wwweegee@gmail.com>
2025-10-18 16:33:47 -0400

    Added timeout function to properly allow client.dll to load, along with helpful message boxes.

--- SniperPOV/SniperPOV/main.cpp
+++ SniperPOV/SniperPOV/main.cpp
@@ -13,13 +13,27 @@
 
 using namespace std;
 
+bool WaitForClientDll(int timeout) { // time in ms
+	HMODULE h = NULL;
+	int time_elapsed = 0;
+	while (time_elapsed < timeout) {
+		h = GetModuleHandle("client.dll");
+		if (h) {
+			return true;
+		}
+		Sleep(500);
+		time_elapsed += 500;
+	}
+	return NULL;
+}
+
 DWORD search_pattern(const char* pattern, const char* mask)
 {
 
 	MODULEINFO info = {};
 	auto hmod = GetModuleHandle("client.dll");
 	if (hmod == nullptr) {
-		MessageBox(NULL, "client.dll couldn't load yet (probably code error, contact dev)", "Sniper POV", MB_SYSTEMMODAL);
+		MessageBox(NULL, "client.dll not loaded yet (code error, contact dev)", "Sniper POV", MB_SYSTEMMODAL);
 		return NULL;
 	}
 	GetModuleInformation(GetCurrentProcess(), hmod, &info, sizeof(MODULEINFO));
@@ -72,18 +86,32 @@ bool __fastcall hInCond(void* ecx, void* edx, ETFCond cond) {
 
 DWORD WINAPI entry(LPVOID lpparam)
 {
-	// ISSUE FIX: search_pattern can't load client.dll the first call, but it works every time after that
-	// A dummy call fixes this (I tried a timeout method, but that didn't work for some reason.)
-	auto dummy = search_pattern("\x55", "x");
+	// Allow client.dll to load (60000 ms / 60 s)
+	if (!WaitForClientDll(60000)) {
+		MessageBox(NULL, "Failed to hook!\nLoading Client.dll exceeded timeout limit!\n(try again, or contact dev)", "Sniper POV", MB_SYSTEMMODAL);
+		return -1;
+	}
 
 	auto sig = search_pattern("\x55\x8B\xEC\x83\xEC\x08\x56\x57\x8B\x7D\x08\x8B\xF1\x83\xFF\x20", "xxxxxxxxxxxxxxxx");
+	if (sig == NULL) {
+		MessageBox(NULL, "Failed to hook!\nSIG pattern could not be found\n(an update may have broken it, contact dev)", "Sniper POV", MB_SYSTEMMODAL);
+		return -1;
+	}
 
 	// Find the CALL instructions to the incond and add 5 to get the address they will be returning to
 
 	// UPDATED SIGNATURES POST-OCTOBER 18th 2025
 	wearable_draw = search_pattern("\xE8????\x84\xC0\x0F\x85????\x6A\x03\x8B\xCB\xE8????\x84\xC0\x0F\x84????", "x????xxxx????xxxxx????xxxx????") + 5;
+	if (wearable_draw == NULL) {
+		MessageBox(NULL, "Failed to hook!\nWearable_draw pattern could not be found\n(an update may have broken it, contact dev)", "Sniper POV", MB_SYSTEMMODAL);
+		return -1;
+	}
 
 	player_draw = search_pattern("\xE8????\x84\xC0\x74?\x5E\x32\xC0\x5B\xC3", "x????xxx?xxxxx") + 5;
+	if (player_draw == NULL) {
+		MessageBox(NULL, "Failed to hook!\nPlayer_draw pattern could not be found\n(an update may have broken it, contact dev)", "Sniper POV", MB_SYSTEMMODAL);
+		return -1;
+	}
 
 	oInCond= (tInCond)(sig);
 
@@ -102,10 +130,11 @@ DWORD WINAPI entry(LPVOID lpparam)
 
 
 	if (error != NO_ERROR) {
-		MessageBox(NULL, "Failed to hook!", "Sniper POV", MB_SYSTEMMODAL);
+		MessageBox(NULL, "Failed to hook!\n(unclear what went wrong, contact dev)", "Sniper POV", MB_SYSTEMMODAL);
 		return -1;
 	}
 
+	MessageBox(NULL, "Successful hook!", "Sniper POV", MB_SYSTEMMODAL);
 	return 0;
 }
 
